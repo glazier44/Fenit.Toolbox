f7dfc67c842a268d091b789e815ececc8af5d581
Tomek Kwiek <kwiek_tomasz@hotmail.com>
2019-01-10 18:16:58 +0100

    new project

--- Fenit.Toolbox.Core/Extension/SystemExtension.cs
+++ Fenit.Toolbox.Core/Extension/SystemExtension.cs
@@ -1,35 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data.Entity.Validation;
 using System.Globalization;
 using System.Linq;
-using System.Text;
 
 namespace Fenit.Toolbox.Core.Extension
 {
     public static class SystemExtension
     {
-        public static void CatchDbEntityValidationException(Exception ex, string source)
-        {
-            var e = ex as DbEntityValidationException;
-            if (e != null)
-            {
-                var result = new StringBuilder();
-                foreach (var eve in e.EntityValidationErrors)
-                {
-                    result.Append(
-                        $"Entity of type \"{eve.Entry.Entity.GetType().Name}\" in state \"{eve.Entry.State}\" has the following validation errors:");
-                    result.Append(Environment.NewLine);
-                    result.Append(
-                        eve.ValidationErrors.Select(
-                            ve => $"- Property: \"{ve.PropertyName}\", Error: \"{ve.ErrorMessage}\""));
-                    result.Append(Environment.NewLine);
-                }
-
-                // LoggerManager.Log(source, result.ToString());
-            }
-        }
-
         public static TimeSpan Sum<TSource>(this IEnumerable<TSource> source, Func<TSource, TimeSpan> selector)
         {
             return source.Select(selector).Aggregate(TimeSpan.Zero, (t1, t2) => t1 + t2);
@@ -128,8 +105,5 @@ namespace Fenit.Toolbox.Core.Extension
             if (dt != null) return (DateTime) dt;
             return DateTime.MinValue;
         }
-
-
-
     }
 }
--- Fenit.Toolbox.EntityFramework/QueryableExtensions.cs
+++ Fenit.Toolbox.EntityFramework/QueryableExtensions.cs
@@ -6,7 +6,7 @@ using System.Reflection;
 using Fenit.Toolbox.Core.Enum;
 using LinqKit;
 
-namespace Fenit.Toolbox.Core.Extension
+namespace Fenit.Toolbox.EntityFramework
 {
     public static class QueryableExtensions
     {
@@ -15,7 +15,7 @@ namespace Fenit.Toolbox.Core.Extension
             string orderByMember,
             Order direction)
         {
-            var queryElementTypeParam = Expression.Parameter(typeof (T));
+            var queryElementTypeParam = Expression.Parameter(typeof(T));
 
             Expression memberAccess = null;
 
@@ -23,9 +23,7 @@ namespace Fenit.Toolbox.Core.Extension
             {
                 memberAccess = queryElementTypeParam;
                 foreach (var property in orderByMember.Split('.'))
-                {
                     memberAccess = Expression.PropertyOrField(memberAccess, property);
-                }
             }
             else
             {
@@ -35,9 +33,9 @@ namespace Fenit.Toolbox.Core.Extension
             var keySelector = Expression.Lambda(memberAccess, queryElementTypeParam);
 
             var orderBy = Expression.Call(
-                typeof (Queryable),
+                typeof(Queryable),
                 direction == Order.Asc ? "OrderBy" : "OrderByDescending",
-                new[] {typeof (T), memberAccess.Type},
+                new[] {typeof(T), memberAccess.Type},
                 query.Expression,
                 Expression.Quote(keySelector));
 
@@ -46,27 +44,21 @@ namespace Fenit.Toolbox.Core.Extension
 
         public static IQueryable<T> Filter<T>(this IQueryable<T> query, string search)
         {
-            if (string.IsNullOrEmpty(search))
-            {
-                return query;
-            }
-            var properties = typeof (T).GetProperties().Where(p =>
-                p.PropertyType == typeof (String) &&
-                p.CustomAttributes.All(w => w.AttributeType != typeof (NotMappedAttribute)));
+            if (string.IsNullOrEmpty(search)) return query;
+            var properties = typeof(T).GetProperties().Where(p =>
+                p.PropertyType == typeof(string) &&
+                p.CustomAttributes.All(w => w.AttributeType != typeof(NotMappedAttribute)));
 
             var predicate = PredicateBuilder.False<T>();
-            foreach (var property in properties)
-            {
-                predicate = predicate.Or(CreateLike<T>(property, search));
-            }
+            foreach (var property in properties) predicate = predicate.Or(CreateLike<T>(property, search));
             return query.AsExpandable().Where(predicate);
         }
 
         private static Expression<Func<T, bool>> CreateLike<T>(PropertyInfo prop, string value)
         {
-            var parameter = Expression.Parameter(typeof (T), "f");
+            var parameter = Expression.Parameter(typeof(T), "f");
             var propertyAccess = Expression.MakeMemberAccess(parameter, prop);
-            var like = Expression.Call(propertyAccess, "Contains", null, Expression.Constant(value, typeof (string)));
+            var like = Expression.Call(propertyAccess, "Contains", null, Expression.Constant(value, typeof(string)));
 
             return Expression.Lambda<Func<T, bool>>(like, parameter);
         }

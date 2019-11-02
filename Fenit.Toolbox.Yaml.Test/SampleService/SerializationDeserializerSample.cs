c593da7d444804910ddb0b080e314b746b8cf039
Tomek Kwiek <kwiek_tomasz@hotmail.com>
2019-11-02 21:27:52 +0100

    Add yaml

---
+++ Fenit.Toolbox.Yaml.Test/SampleService/SerializationDeserializerSample.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using Fenit.Toolbox.Yaml.Test.Models;
+using InstallPackageLib.ProgramsType;
+using YamlDotNet.Serialization;
+
+namespace Fenit.Toolbox.Yaml.Test.SampleService
+{
+    public class SerializationDeserializerSample
+    {
+
+        public void First()
+        {
+            var deserializer = new YamlDotNet.Serialization.Deserializer();
+            var dict = deserializer.Deserialize<Dictionary<string, string>>("hello: world");
+            Console.WriteLine(dict["hello"]);
+        }
+
+        public void ClassDes()
+        {
+            var yamlInput = 
+@"- Name: Oz-Ware
+  PhoneNumber: 123456789";
+            var deserializer = new DeserializerBuilder()
+                .Build();
+
+            var contacts = deserializer.Deserialize<List<Contact>>(yamlInput);
+            Console.WriteLine(contacts[0]);
+        }
+
+        public void Test()
+        {
+            var yaml = @"- Programs:
+  - Name: ApplicationHost
+  - Name: SerwerSerializacji
+  - Name: CmdServer
+  - Name: Mobmago
+  - Name: AppLoader
+  Name: Serwerowe
+- Programs:
+  - Name: KomoraCE
+  - Name: ZatowCE
+  - Name: KodyKreskowe
+  - Name: Inwentaryzacja
+  - Name: MagazynCE
+  - Name: StacjaWydań
+  - Name: StacjaWydań
+  Name: Mobilny
+- Programs:
+  - Name: Zwroty
+  - Name: DIZ
+  Name: Desktop
+- Programs:
+  - Name: Komora
+  - Name: KodyKreskowe
+  Name: Android
+- Programs:
+  - Name: HelpTool
+  Name: Inne";
+
+            var deserializer = new DeserializerBuilder()
+                .Build();
+
+            var contacts = deserializer.Deserialize<List<ProgramType>>(yaml);
+            Console.WriteLine(contacts);
+        }
+    }
+}

6b3c67f53cfd4df0f7a4b2de0dfb496c8d0e3750
tkwiek <kwiek.t@gmail.com>
2020-01-23 11:41:01 +0100

    Update sitrn&file extension

---
+++ Fenit.Toolbox.Core/Extension/FileExtension.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace Fenit.Toolbox.Core.Extension
+{
+    public static class FileExtension
+    {
+        public static string GetFileBasedExtension(this string path, string ext)
+        {
+            return GetExeFileName(Directory.GetFiles(path, $"*.{ext}"));
+        }
+        public static string GetFileBasedExtensionsFile(this string path, params string[] ext)
+        {
+            foreach (var el in ext)
+            {
+                var res= path.GetFileBasedExtension(el);
+                if (!string.IsNullOrEmpty(res))
+                {
+                    return res;
+                }
+            }
+            return string.Empty;
+        }
+        public static string GetVersion(this string path, bool removeFeature)
+        {
+            var versionInfo = FileVersionInfo.GetVersionInfo(path);
+            if (removeFeature)
+            {
+                var sp = versionInfo.ProductVersion.Split('-');
+                return sp[0];
+            }
+            return versionInfo.ProductVersion;
+        }
+
+
+        public static bool HasFileExtension(this string path, params string[] ext)
+        {
+            foreach (var el in ext)
+            {
+                var res = path.GetFileBasedExtension(el);
+                if (!string.IsNullOrEmpty(res))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public static string GetFileWithoutExtension(this string fileName)
+        {
+            if (string.IsNullOrEmpty(fileName))
+                return string.Empty;
+
+            return Path.GetFileNameWithoutExtension(fileName);
+        }
+
+        private static string GetExeFileName(string[] exeFiles)
+        {
+            foreach (var exeFile in exeFiles.OrderBy(w => w))
+            {
+                if (exeFile.EndsWith(".vshost.exe", StringComparison.InvariantCultureIgnoreCase))
+                    continue;
+
+                return Path.GetFileName(exeFile);
+            }
+
+            return string.Empty;
+        }
+    }
+}
